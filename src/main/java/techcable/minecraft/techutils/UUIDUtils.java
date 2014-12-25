899817078302f12986ad58e14267b2cfaaec1864
Techcable <Techcable@outlook.com>
2014-12-25 00:13:11 -0700

    Switch to turt2live uuid library

--- src/main/java/techcable/minecraft/techutils/UUIDUtils.java
+++ src/main/java/techcable/minecraft/techutils/UUIDUtils.java
@@ -5,31 +5,30 @@ import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 
-import techcable.minecraft.uuidfetcher.UUIDFetcher;
+import com.turt2live.uuid.CachingServiceProvider;
+import com.turt2live.uuid.PlayerRecord;
+import com.turt2live.uuid.ServiceProvider;
+import com.turt2live.uuid.turt2live.v2.ApiV2Service;
 
 public class UUIDUtils {
+	static {
+		UUID_PROVIDER = new CachingServiceProvider(new ApiV2Service());
+	}
 	private UUIDUtils() {}
-	
+	private static final ServiceProvider UUID_PROVIDER;
 	public static String getName(UUID uuid) {
-		try {
-			return UUIDFetcher.getName(uuid);
-		} catch (Exception ex) {
-			if (TechUtils.isDebug()) {
-				TechUtils.getLog().log(Level.WARNING, "Unable to Fetch Name through UUIDFetcher", ex);
-			}
-			return Bukkit.getOfflinePlayer(uuid).getName();
+		PlayerRecord player = UUID_PROVIDER.doLookup(uuid);
+		if (player == null) {
+			return null;
 		}
+		return player.getName();
 	}
 	
-	@SuppressWarnings("deprecation")
 	public static UUID getUUID(String name) {
-		try {
-			return UUIDFetcher.getUUID(name);
-		} catch (Exception ex) {
-			if (TechUtils.isDebug()) {
-				TechUtils.getLog().log(Level.WARNING, "Unable to Fetch uuid through UUIDFetcher", ex);
-			}
-			return Bukkit.getOfflinePlayer(name).getUniqueId();
+		PlayerRecord player = UUID_PROVIDER.doLookup(name);
+		if (player == null) {
+			return null;
 		}
+		return player.getUuid();
 	}
 }
