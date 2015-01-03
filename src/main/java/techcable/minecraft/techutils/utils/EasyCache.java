75bf0120cefd5be0a9553bd5e6a6b81c9cc7c423
Techcable <Techcable@outlook.com>
2015-01-03 11:57:17 -0700

    Catch Any Throwable This was causing a no such method error in TechUtils initializer

--- src/main/java/techcable/minecraft/techutils/utils/EasyCache.java
+++ src/main/java/techcable/minecraft/techutils/utils/EasyCache.java
@@ -10,7 +10,7 @@ public abstract class EasyCache<K, V> {
     public static <K, V> EasyCache<K, V> makeCache(Loader<K, V> loader) {
     	try {
     		return new Guava17EasyCache<K, V>(loader);
-    	} catch (Exception ex) { //Guava wrong version
+    	} catch (Throwable ex) { //Guava wrong version
     		return new AlwaysLoadEasyCache<K, V>(loader);
     	}
     }
